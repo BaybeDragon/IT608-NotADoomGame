2059897244de2d9ee9a6a34618c1f903e31e24b4
BaybeDragon <thomaspickford@outlook.com>
2017-09-19 19:01:04 +1200

    Unity Project
    Uploading the unity game project

---
+++ FPShooter/Assets/GunShoot.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShoot : MonoBehaviour {
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public Transform gunEnd;
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.7f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
+    EnemyHealth health;
+
+
+
+	// Use this for initialization
+	void Start () {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKey(KeyCode.Mouse0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit hit;
+            laserLine.SetPosition(0, gunEnd.position);
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+                if (hit.collider.tag == "Enemy")
+                {
+                    health = hit.collider.GetComponent<EnemyHealth>();
+                    if(health != null)
+                    {
+                        health.Damage(gunDamage);
+                    }
+                    if (hit.rigidbody != null)
+                    {
+                        hit.rigidbody.AddForce(-hit.normal * hitForce);
+                    }
+                }
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }
+	}
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        //laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+
+    private void GunRecoil()
+    {
+
+    }
+}
---
+++ FPShooter/Assets/SpriteLookAtPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLookAtPlayer : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 play = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z);
+        transform.LookAt(play);
+        
+    }
+}
