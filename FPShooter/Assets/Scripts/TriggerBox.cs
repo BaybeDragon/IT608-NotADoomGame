f05f9f5f433944b5dad7d58218da348f489b3d52
BaybeDragon <thomaspickford@outlook.com>
2017-10-22 00:42:26 +1300

    Baddie

---
+++ FPShooter/Assets/Scripts/TriggerBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBox : MonoBehaviour {
+    public GameObject baddie;
+    BaddieController cont;
+	// Use this for initialization
+	void Start () {
+        cont = baddie.GetComponent<BaddieController>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag("Player"))
+        {
+            cont.Triggered();
+        }
+	}
+}
