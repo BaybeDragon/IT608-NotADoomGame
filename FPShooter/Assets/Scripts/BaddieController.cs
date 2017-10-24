5fbbb9e7036f817fdc3ea33467277eabd560e605
BaybeDragon <thomaspickford@outlook.com>
2017-10-24 14:50:43 +1300

    Black Hand

--- FPShooter/Assets/Scripts/BaddieController.cs
+++ FPShooter/Assets/Scripts/BaddieController.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaddieController : MonoBehaviour {
-    public  virtual 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    public GameObject player;
+    bool triggered;
+    Animator anim;
+    public float fireRateMin = 0.25f;
+    public float fireRateMax = 1.5f;
+    private float nextFire = 0f;
+
+    // Use this for initialization
+    void Start () {
+        triggered = false;
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.forward = player.transform.forward;
+        if (triggered)
+        {
+            anim.SetBool("BaddieTriggered", true);
+            
+            if(Time.time > nextFire)
+            {
+                anim.SetTrigger("BaddieShoot");
+                nextFire = Time.time + Random.Range(fireRateMin, fireRateMax);
+            }
+        }
 	}
 
     public void Triggered()
     {
+        triggered = true;
+        nextFire = Time.time + Random.Range(fireRateMin, fireRateMax);
 
     }
 
