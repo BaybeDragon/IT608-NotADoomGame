01f5663e869f8c464899acd6dafd01a7f8999b7b
BaybeDragon <32088008+BaybeDragon@users.noreply.github.com>
2017-11-03 14:07:58 +1300

    AmmoPickups Look at player

--- FPShooter/Assets/Scripts/PickupsLookAtPlayer.cs
+++ FPShooter/Assets/Scripts/PickupsLookAtPlayer.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PickupsLookAtPlayer : MonoBehaviour {
+    public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.LookAt(player.transform);	
 	}
 }
